84e3b43b45b72064098aa9940c88c86095dcf6e3
roblotter <roblotter1@gmail.com>
2019-06-17 00:14:01 +0300

    Add Adaptable Blotter page

---
+++ src/cms/preview-templates/AdaptableBlotterPagePreview.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { AdaptableBlotterPageTemplate } from '../../templates/adaptable-blotter-page';
+
+const AdaptableBlotterPagePreview = ({ entry, widgetFor }) => (
+  <AdaptableBlotterPageTemplate
+    title={entry.getIn(['data', 'title'])}
+    content={widgetFor('body')}
+  />
+);
+
+AdaptableBlotterPagePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func
+  }),
+  widgetFor: PropTypes.func
+};
+
+export default AdaptableBlotterPagePreview;
