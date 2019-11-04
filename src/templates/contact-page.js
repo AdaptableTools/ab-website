9ddbc9b9a3e6cd653a3ac7ac073b685b14dfc527
roblotter <roblotter1@gmail.com>
2019-11-04 15:20:55 +0200

    update

--- src/templates/contact-page.js
+++ src/templates/contact-page.js
@@ -1,24 +1,148 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { navigate } from 'gatsby-link'
 import Content, { HTMLContent } from '../components/Content'
 import Layout from '../components/Layout'
 
-const ContactPageTemplate =  ({ title, content,contentComponent }) => {
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
+
+
+
+export class ContactForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { isValidated: false }
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...this.state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute('action')))
+      .catch(error => alert(error))
+  }
+
+  render() {
+    return (
+      
+              <form
+                name="contact"
+                method="post"
+                action="/contact/thanks/"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+                onSubmit={this.handleSubmit}
+              >
+                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                <input type="hidden" name="form-name" value="contact" />
+                <div hidden>
+                  <label>
+                    Don’t fill this out:{' '}
+                    <input name="bot-field" onChange={this.handleChange} />
+                  </label>
+                </div>
+                <div className="field">
+                  <label className="label" htmlFor={'name'}>
+                    Your name
+                  </label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type={'text'}
+                      name={'name'}
+                      onChange={this.handleChange}
+                      id={'name'}
+                      required={true}
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label" htmlFor={'email'}>
+                    Email
+                  </label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type={'email'}
+                      name={'email'}
+                      onChange={this.handleChange}
+                      id={'email'}
+                      required={true}
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label" htmlFor={'message'}>
+                    Message
+                  </label>
+                  <div className="control">
+                    <textarea
+                      className="textarea"
+                      name={'message'}
+                      onChange={this.handleChange}
+                      id={'message'}
+                      required={true}
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <button className="button is-link" type="submit">
+                    Send
+                  </button>
+                </div>
+              </form>
+    )
+  }
+}
+
+
+export const ContactPageTemplate =  ({ title, headerImage, content,contentComponent }) => {
   const PageContent = contentComponent || Content
-  return <Layout>
-    <section className="section">
+  return <section className=" section--gradient">
       <div className="container">
-        <div className="content">
-          <h1>{title}</h1>
-          <PageContent className="content" content={content} />
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            
+              
+
+              {headerImage? <div
+      className="full-width-image-container margin-top-0"
+      style={{
+        backgroundImage: `url(${
+          !!headerImage.childImageSharp ? headerImage.childImageSharp.fluid.src : headerImage
+        })`,
+      }}
+    ><h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                {title}
+              </h2></div>: null}
+
+              <PageContent className="content" content={content} />
+              <ContactForm />
+            
+          </div>
         </div>
       </div>
     </section>
-  </Layout>
 }
 
 ContactPageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
+  headerImage: PropTypes.string,
   content: PropTypes.string,
   contentComponent: PropTypes.func,
 }
@@ -32,6 +156,7 @@ const ContactPage = ({ data }) => {
       <ContactPageTemplate
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
+        headerImage={post.frontmatter.headerimage}
         content={post.html}
       />
     </Layout>
@@ -49,6 +174,13 @@ export const ContactPageQuery = graphql`
     markdownRemark(id: { eq: $id }) {
       html
       frontmatter {
+        headerimage {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
         title
       }
     }
